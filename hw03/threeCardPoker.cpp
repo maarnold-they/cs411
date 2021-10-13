f06982ea8a407bd7df7498c5566dc5fe049ac0d1
Millard A Arnold V <maarnold7@alaska.edu>
2021-10-13 15:23:04 -0800

    Created source file for threeCardPoker

---
+++ hw03/threeCardPoker.cpp
@@ -0,0 +1,19 @@
+/*
+  Millard A. Arnold V
+  threeCardPoker.h
+  2021-10-13
+
+  threeCardPoker header file defines functions for Three Card Poker analysis
+*/
+
+#include "threeCardPoker.h"
+
+std::vector<Card> makeDeck()
+{
+  return {Card(0,0)};
+}
+
+void computeHand(std::map<std::string, int> quants, std::vector<Card> hand)
+{
+  
+}
