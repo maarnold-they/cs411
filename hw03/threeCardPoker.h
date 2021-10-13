11ff707d49d2e45a59badc161b32aefa42ca45ed
Millard A Arnold V <maarnold7@alaska.edu>
2021-10-13 15:19:26 -0800

    Created header for threeCardPoker

---
+++ hw03/threeCardPoker.h
@@ -0,0 +1,22 @@
+/*
+  Millard A. Arnold V
+  threeCardPoker.h
+  2021-10-13
+
+  threeCardPoker header file defines functions for Three Card Poker analysis
+*/
+
+#ifndef THREE_CARD_POKER_H
+#define THREE_CARD_POKER_H
+
+#include "card.h"
+
+#include <vector>
+#include <map>
+#include <string>
+
+std::vector<Card> makeDeck();
+
+void computeHand(std::map<std::string, int> quants, std::vector<Card> hand);
+
+#endif
