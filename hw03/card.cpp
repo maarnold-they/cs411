82284ef54dbaf16fb38624e2cdcf9979fe4e8e41
Millard A Arnold V <maarnold7@alaska.edu>
2021-10-13 15:00:40 -0800

    Card.cpp blank functions

---
+++ hw03/card.cpp
@@ -0,0 +1,31 @@
+/*
+  Millard A. Arnold V
+  card.cpp
+  2021-10-13
+
+  Card source file defines cards
+*/
+
+#include "card.h"
+
+bool Card::operator==(const Card& other) const
+{
+  return false;
+}
+bool Card::operator<(const Card& other) const
+{
+  return false;
+}
+bool Card::operator<=(const Card& other) const
+{
+  return false;
+}
+
+int Card::getRank()
+{
+  return 0;
+}
+int Card::getSuit()
+{
+  return 0;
+}
