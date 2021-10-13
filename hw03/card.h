47fb03db92c800a0f1c77bf43d502a37262c21fd
Millard A Arnold V <maarnold7@alaska.edu>
2021-10-13 14:48:08 -0800

    Made header file for card class

---
+++ hw03/card.h
@@ -0,0 +1,44 @@
+/*
+  Millard A. Arnold V
+  card.h
+  2021-10-13
+
+  Card header file defines cards
+*/
+
+#ifndef CARD_H
+#define CARD_H
+
+class Card
+{
+
+//ctors
+public:
+  Card() = delete;
+  Card(int suit, int rank)
+    : _suit(suit), _rank(rank)
+  {}
+
+//big 5 defaulted
+public:
+  ~Card()=default;
+  Card(const Card& other)=default;
+  Card(Card&& other)=default;
+  Card& operator=(const Card& other)=default;
+  Card& operator=(Card&& other)=default;
+
+public:
+  bool operator==(const Card& other) const;
+  bool operator<(const Card& other) const;
+  bool operator<=(const Card& other) const;
+
+  int getRank();
+  int getSuit();
+
+//data members
+private:
+  int _suit;
+  int _rank;
+};
+
+#endif
